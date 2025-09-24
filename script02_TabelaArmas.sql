3705dd48b9bd818447a7d6e2c9cb55fa0f6b3754
= <=>
2025-09-24 20:43:49 -0300

    Aula 09 - Avaliação Parte 1

---
+++ script02_TabelaArmas.sql
@@ -0,0 +1,27 @@
+﻿BEGIN TRANSACTION;
+CREATE TABLE [TB_ARMAS] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NOT NULL,
+    [Dano] int NOT NULL,
+    CONSTRAINT [PK_TB_ARMAS] PRIMARY KEY ([Id])
+);
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ARMAS]'))
+    SET IDENTITY_INSERT [TB_ARMAS] ON;
+INSERT INTO [TB_ARMAS] ([Id], [Dano], [Nome])
+VALUES (1, 35, N'Arco e Flecha'),
+(2, 33, N'Espada'),
+(3, 31, N'Machado'),
+(4, 30, N'Punho'),
+(5, 34, N'Chicote'),
+(6, 33, N'Foice'),
+(7, 32, N'Cajado');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ARMAS]'))
+    SET IDENTITY_INSERT [TB_ARMAS] OFF;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250924224723_MigracaoArma', N'9.0.9');
+
+COMMIT;
+GO
+
